f0f2d32644219c8b7634e1f06eec6d90993a7f92
Camila Perez <perez9camila@gmail.com>
2023-03-02 19:10:07 -0300

    agrego archivos de la primer clase con ejercicios resueltos

---
+++ Clase 1 Introducción a Algoritmos y Java/Ejercicio1.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+/*a. Utilizando la sentencia while, imprima todos los números entre 2 variables “a”
+y “b”. Su código puede arrancar (por ejemplo):
+int numeroInicio = 5;
+int numeroFin = 14;
+// Se deberían mostrar los números:
+5,6,7,8,9,10,11,12,13,14
+b. A lo anterior, solo muestre los números pares
+c. A lo anterior, con una variable extra, elija si se deben mostrar los números
+pares o impares
+d. Utilizando la sentencia for, hacer lo mismo que en (b) pero invirtiendo el orden */
+
+public class Ejercicio1{
+     
+    public static void main (String[] args) {
+        
+        int numero_inicio = 5;
+        int numero_fin = 14;
+        Scanner pares_impares = new Scanner(System.in);
+
+        /*punto A: todos los numeros del 5 al 14 inclusive
+        while (numero_inicio <= numero_fin){
+            System.out.println(numero_inicio);
+            numero_inicio ++;
+        }*/
+
+        /*punto B: todos los numeros PARES del 5 al 14 inclusive
+        while (numero_inicio <= numero_fin){
+            if (numero_inicio %2 == 0){
+            System.out.println(numero_inicio);
+            }
+            numero_inicio ++;
+        }*/
+
+        /*punto C: elegir entre mostrar impares o pares */
+        System.out.println("Desea mostrar numeros impares o pares?: ");
+        String respuesta_usuario = pares_impares.nextLine();
+        
+
+        if (respuesta_usuario == "pares"){
+            while (numero_inicio <= numero_fin){
+                if (numero_inicio %2 == 0){
+                    System.out.println(numero_inicio);
+                } else System.out.println(numero_inicio);
+            }
+        }
+
+
+
+
+
+
+
+    }
+
+    
+}
